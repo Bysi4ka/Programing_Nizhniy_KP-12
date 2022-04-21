ba36f64951e14ec3df8740281c84d451bf26d7de
Bysi4ka <ivansuper414@gmail.com>
2022-04-21 20:34:43 +0300

    ready To Check

---
+++ lab4/ConnectToSQLServer/ConnectToSQLServer/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System.Windows;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+using System.Windows.Controls;
+
+namespace ConnectToSQLServer
+{
+    public partial class MainWindow : Window
+    {
+        string connectionString = null;        
+        SqlConnection connection = null;
+        SqlCommand command;
+        SqlDataAdapter adapter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            //MessageBox.Show(connectionString);
+            GetStudentsData();
+            GetSubjectsData();
+            GetGroupsData();
+            GetAllMarks();
+        }
+
+        private void GetAndDhowData(string SQLQuery, DataGrid dataGrid)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            command = new SqlCommand(SQLQuery, connection);
+            adapter = new SqlDataAdapter(command);
+            DataTable Table = new DataTable();
+            adapter.Fill(Table);
+            dataGrid.ItemsSource = Table.DefaultView;
+            connection.Close();            
+        }
+
+        private void GetStudentsData()
+        {
+            string sqlQ = "SELECT IDStudent as [№], " +
+                       "StudentSurname as Прізвище, " +
+                       "StudentName as [ім'я], " +
+                       "StudentSecondName as [по батькові] " +
+                    "FROM Students;";
+            try
+            {
+                GetAndDhowData(sqlQ, StudentsDG);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void GetSubjectsData()
+        {
+            string sqlQ = "SELECT IDSubject as [№], " +
+                       "SubjectName as [Назва дисципліни] " +                       
+                    "FROM Subjects;";
+            try
+            {
+                GetAndDhowData(sqlQ, SubjectsDG);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void GetGroupsData()
+        {
+            string sqlQ = "SELECT IDGroup as [№], " +
+                       "GroupName as Назва " +                       
+                    "FROM Groups;";
+            try
+            {
+                GetAndDhowData(sqlQ, GroupsDG);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void GetAllMarks()
+        {
+            string sqlQ = "SELECT dbo.Students.StudentSurname AS Прізвище, " +
+                "dbo.Students.StudentName AS [Ім'я], " +
+                "dbo.Groups.GroupName AS Група, " +
+                "dbo.Subjects.SubjectName AS [Назва предмету], " +
+                "dbo.Marks.Mark AS Оцінка " +
+                "FROM dbo.Students INNER JOIN " +
+                "dbo.Marks ON dbo.Students.IDStudent = dbo.Marks.IDStudent INNER JOIN " +
+                "dbo.StudentsInGroups ON dbo.Students.IDStudent = dbo.StudentsInGroups.IDStudent INNER JOIN " +
+                "dbo.Groups ON dbo.StudentsInGroups.IDGroup = dbo.Groups.IDGroup INNER JOIN " +
+                "dbo.Subjects ON dbo.Marks.IDSubject = dbo.Subjects.IDSubject " +
+             "ORDER BY Група, Прізвище, [Ім'я]";
+            try
+            {
+                GetAndDhowData(sqlQ, MarksDG);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
---
+++ lab4/lab4/lab4/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace lab4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string connectionString = null;
+        SqlConnection connection = null;
+        SqlCommand command;
+        SqlDataAdapter adapter;
+
+        public MainWindow()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            InitializeComponent();
+            firmbookData();
+            forsaleData();
+            soldData();
+            byerData();
+            sellerData();
+           // string con = "Data Source=IOANPC\MSSQLSERVER1;Initial Catalog=AuctionDB;Integrated Security=True";
+
+        }
+        private void GetAndDhowData(string SQLQuery, DataGrid dataGrid)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            command = new SqlCommand(SQLQuery, connection);
+            adapter = new SqlDataAdapter(command);
+            DataTable Table = new DataTable();
+            adapter.Fill(Table);
+            dataGrid.ItemsSource = Table.DefaultView;
+            connection.Close();
+        }
+        private void firmbookData()
+        {
+            string sqlQ = "SELECT nameofauction as Назва, date as Дата, place as Мiсце, time as Час, specification as Опис FROM dbo.firmbook";
+            try
+            {
+                GetAndDhowData(sqlQ, firmbook);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void forsaleData()
+        {
+            string sqlQ = "SELECT dbo.forsale.nameofauction as [Назва аукцiону], dbo.forsale.numberoflot as [Номер лота], dbo.forsale.startprize as [Стартова цiна], dbo.seller.name as [Iм'я продавця], dbo.seller.surname as [Прiзвище продавця], dbo.forsale.description as [Опис]" + "FROM dbo.forsale " + "INNER JOIN dbo.seller ON dbo.forsale.sellerID = dbo.seller.ID";
+            try
+            {
+                GetAndDhowData(sqlQ, forsale);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void soldData()
+        {
+            string sqlQ = "SELECT dbo.byer.name as [Ім'я покупця], dbo.byer.surname as [Прізвище покупця], dbo.sold.prize as [Ціна продажі], dbo.forsale.description as Опис FROM dbo.sold INNER JOIN dbo.byer ON dbo.sold.byerID = dbo.byer.ID INNER JOIN dbo.forsale ON dbo.sold.ID = dbo.forsale.ID";
+            try
+            {
+                GetAndDhowData(sqlQ, sold);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void byerData()
+        {
+            string sqlQ = "SELECT name as [Ім'я покупця], surname as [Прізвище покупця] FROM dbo.byer";
+            try
+            {
+                GetAndDhowData(sqlQ, byer);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void sellerData()
+        {
+            string sqlQ = "SELECT name as [Ім'я продавця], surname as [Прізвище продавця] FROM dbo.seller";
+            try
+            {
+                GetAndDhowData(sqlQ, seller);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
